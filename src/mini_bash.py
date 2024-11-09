6d30ef27f5c0e5fb2a60c1f2a28537e267651ef1
lamnguyenx <lamfm95@gmail.com>
2024-11-09 10:52:52 +0700

    Support more args

--- src/mini_bash.py
+++ src/mini_bash.py
@@ -8,19 +8,24 @@ import typing as tp
 import subprocess
 
 
-
 ### FUNCTIONS
-def mini_bash(cmd: str) -> tp.Tuple[str]:
-    cmd = 'set -Eeuo pipefail\n' + cmd
+def mini_bash(
+    cmd: str,
+    executable: str = "/bin/bash",
+    shell: bool = True,
+    capture_output: bool = True,
+    text: bool = True,
+) -> tp.Tuple[str]:
+    cmd = "set -Eeuo pipefail\n" + cmd
     Result = subprocess.run(
         cmd,
-        executable     = '/bin/bash',
-        shell          = True,
-        capture_output = True,
-        text           = True,
+        executable=executable,
+        shell=shell,
+        capture_output=capture_output,
+        text=text,
     )
     if Result.returncode != 0:
         raise RuntimeError(
-            '> cant execute:\n' + cmd[:1000] + '\n' + '> stderr:\n' + Result.stderr
+            "> cant execute:\n" + cmd[:1000] + "\n" + "> stderr:\n" + Result.stderr
         )
     return Result.stdout, Result.stderr
